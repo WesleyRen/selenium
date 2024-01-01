84b28a305f4db55d2a34ef072175cb6ae8be9061
Wesley Ren <wesleyren@yahoo.com>
2024-01-01 15:20:58 -0800

    add element locator

--- src/main/java/org/example/SeleniumIntro.java
+++ src/main/java/org/example/SeleniumIntro.java
@@ -1,39 +1,40 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumIntro {
     public static void main(String[] args) {
-        // Invoking Browser.
-        // Chrome - ChromeDriver -> Methods
-        // create ChromeDriver object
+        System.out.println(String.join(" ", args));
+        String driverName = args.length > 0 ? args[0] : "firefox";
+        WebDriver driver;
 
-        // Step 1: Set the path of the driver
-        // step to invoke the driver
-        // Or, using a Selenium manager, but the manager will do
-        System.setProperty("webdriver.chrome.driver", "/Users/wes/Documents/webdriver/chromedriver");
-        WebDriver chromeDriver = new ChromeDriver(); // Can be FirefoxDriver, SafariDriver, etc.
-
-        // Firefox, gecko driver
-        System.setProperty("webdriver.gecko.driver", "/Users/wes/Documents/webdriver/geckodriver");
-        WebDriver firefoxDriver = new FirefoxDriver();
-
-        // MS edge driver -- NM, I don't have EDge browser.
-//        System.setProperty("webdriver.edge.driver", "/Users/wes/Documents/webdriver/msedgedriver");
-//        WebDriver edgeDriver = new EdgeDriver();
-//        edgeDriver.get("https://rahulshettyacademy.com/");
-//        edgeDriver.close();
-
-        WebDriver[] drivers = {chromeDriver, firefoxDriver};
-        for (WebDriver driver : drivers) {
-            driver.get("https://rahulshettyacademy.com/");
-            System.out.println(driver.getTitle());
-            System.out.println(driver.getCurrentUrl());
-            System.out.println(driver.getPageSource());
-            driver.close();
-//            driver.quit(); // close all the associated windows
+        if (driverName.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "/Users/wes/Documents/webdriver/chromedriver");
+            driver = new ChromeDriver();
+        } else if (driverName.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "/Users/wes/Documents/webdriver/geckodriver");
+            driver = new FirefoxDriver();
+        } else {
+            System.setProperty("webdriver.edge.driver", "/Users/wes/Documents/webdriver/msedgedriver");
+            driver = new EdgeDriver(); // Microsoft Edge, need Edge browser installed to work.
         }
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        try {
+            driver.findElement(By.id("inputUsername")).sendKeys("wes");
+            driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+            driver.findElement(By.className("submit")).click();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        driver.close();
+//            driver.quit(); // quite the driver, which closes all the associated windows
+        System.exit(0);
     }
 }
