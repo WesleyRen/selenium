a89c42eb6342d78068dbfb7cd7c04c20f3c281df
Wesley Ren <wesleyren@yahoo.com>
2024-01-01 21:09:33 -0800

    add a method to get the temporary password

--- src/main/java/org/example/Locator2.java
+++ src/main/java/org/example/Locator2.java
@@ -34,12 +34,11 @@ public class Locator2 {
             driver = new EdgeDriver(); // Microsoft Edge, need Edge browser installed to work.
         }
 
+        String password = getPassword(driver);
+
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.findElement(By.id("inputUsername")).sendKeys(username);
-        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.name("inputPassword")).sendKeys(password);
         driver.findElement(By.className("submit")).click();
 
         Thread.sleep(2000);
@@ -50,8 +49,31 @@ public class Locator2 {
         Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(),
                 "Hello " + username + ",");
 
+        driver.findElement(By.xpath("//*[text()='Log Out']")).click();
 
         driver.close();
         System.exit(0);
+    }
+
+    public static String getPassword(WebDriver driver) throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        String passwordText =driver.findElement(By.cssSelector("form p")).getText();
+//Please use temporary password 'rahulshettyacademy' to Login.
+        String[] passwordArray = passwordText.split("'");
+// String[] passwordArray2 = passwordArray[1].split("'");
+// passwordArray2[0]
+
+        String password = passwordArray[1].split("'")[0];
+
+        return password;
+
+//0th index - Please use temporary password
+//1st index - rahulshettyacademy' to Login.
+
+//0th index - rahulshettyacademy
+//1st index - to Login.
     }
 }
