2aa0d49a2ffed95ecf94b2ce957579be6d570710
Wesley Ren <wesleyren@yahoo.com>
2024-01-01 17:15:32 -0800

    add wait and check for error

--- src/main/java/org/example/SeleniumIntro.java
+++ src/main/java/org/example/SeleniumIntro.java
@@ -6,10 +6,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
+import java.util.Set;
+
 public class SeleniumIntro {
+    private static final String defaultDriver = "chrome";
+    private static final Set<String> allDrivers = Set.of("chrome", "firefox", "edge");
+
     public static void main(String[] args) {
         System.out.println(String.join(" ", args));
-        String driverName = args.length > 0 ? args[0] : "firefox";
+        String driverName = defaultDriver;
+        if (args.length > 0 && allDrivers.contains(args[0])) {
+            driverName = args[0];
+        }
         WebDriver driver;
 
         if (driverName.equals("chrome")) {
@@ -27,11 +36,14 @@ public class SeleniumIntro {
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
         try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
             driver.findElement(By.id("inputUsername")).sendKeys("wes");
-            driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+            driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy-bad");
             driver.findElement(By.className("submit")).click();
+            System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
         driver.close();
 //            driver.quit(); // quite the driver, which closes all the associated windows
